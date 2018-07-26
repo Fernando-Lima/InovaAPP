5165008b314d73403ab0c18a1dd8dbb0a87ba3f3
Fernando-Lima <fernandosupweb@gmail.com>
2018-07-26 11:14:08 -0300

    tela de login

--- app/src/main/java/com/example/fernando/inovaapp/DB/BancoDados.java
+++ app/src/main/java/com/example/fernando/inovaapp/DB/BancoDados.java
@@ -1,22 +1,36 @@
 package com.example.fernando.inovaapp.DB;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 public class BancoDados {
     private static final String NOME_BANCO = "baseDados";
-    private static final String NOME_TABELA = "tbl";
+    private static final String NOME_TABELA_USUARIO = "tbl_usuario";
     private static final int VERSAO_BANCO = 1;
 
-    private static final String[] SCRIPT_DATABASE_DELETE = new String[0];
-    private static final String[] SCRIPT_DATABASE_CREATE =  new String[0];
+    private static final String[] SCRIPT_DATABASE_DELETE = new String[4];
+    private static final String[] SCRIPT_DATABASE_CREATE =  new String[4];
 
     private static SQLiteDatabase sqLiteDatabase;
 
     public static SQLiteDatabase getDb(Context ctx){
-        SCRIPT_DATABASE_DELETE [0] = "DROP TABLE IF EXISTS" + NOME_TABELA;
-        SCRIPT_DATABASE_CREATE [0] = "CREATE TABLE " + NOME_TABELA + "(_id_usuario integer primary key, nome text)";
+        SCRIPT_DATABASE_DELETE [0] = "DROP TABLE IF EXISTS" + NOME_TABELA_USUARIO;
+        SCRIPT_DATABASE_CREATE [0] = "CREATE TABLE " + NOME_TABELA_USUARIO + "(_id_usuario integer primary key, " +
+                "nome text not null, email text not null)";
 
+
+        try {
+            if(sqLiteDatabase == null){
+                SQLiteHelper sqLiteHelper = new SQLiteHelper(ctx,NOME_BANCO, SCRIPT_DATABASE_CREATE, SCRIPT_DATABASE_DELETE ,VERSAO_BANCO);
+                sqLiteDatabase = sqLiteHelper.getWritableDatabase();
+                Log.i("banco","BANCO DE DADOS CRIADO COM SUCESSO!");
+            }
+        }catch (SQLException ex){
+            ex.printStackTrace();
+        }
         return sqLiteDatabase;
+
     }
 }
---
+++ app/src/main/java/com/example/fernando/inovaapp/DB/SQLiteHelper.java
@@ -0,0 +1,39 @@
+package com.example.fernando.inovaapp.DB;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+    private String[] scriptSQLCreate;
+    private String[] scriptSQLDelete;
+
+    public SQLiteHelper(Context context, String name, String[] scriptSQLCreate, String[] scriptSQLDelete, int version) {
+        super(context, name, null, version);
+        this.scriptSQLCreate = scriptSQLCreate;
+        this.scriptSQLDelete = scriptSQLDelete;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        int qtdScript = scriptSQLCreate.length;
+
+        for ( int i = 0; i < qtdScript; i++ ){
+            sqLiteDatabase.execSQL(scriptSQLCreate[i]);
+            Log.i("banco","SQLiteHelper banco criado com sucesso");
+        }
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        int qtdScript = scriptSQLCreate.length;
+        for (int i = 0; i < qtdScript; i++) {
+            sqLiteDatabase.execSQL(scriptSQLDelete[i]);
+            Log.i("banco","Drop db");
+        }
+        onCreate(sqLiteDatabase);
+        Log.i("banco","update db");
+    }
+}
