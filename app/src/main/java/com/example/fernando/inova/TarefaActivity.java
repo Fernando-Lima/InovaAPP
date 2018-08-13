3178abb8a36161372119fdebb55237654158226b
Fernando-Lima <fernandosupweb@gmail.com>
2018-08-13 13:44:08 -0300

    Incluído Timepicker e textview no layout para hora e min

--- app/src/main/java/com/example/fernando/inova/TarefaActivity.java
+++ app/src/main/java/com/example/fernando/inova/TarefaActivity.java
@@ -1,12 +1,13 @@
 package com.example.fernando.inova;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TimePicker;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -14,10 +15,11 @@ import java.util.Calendar;
 
 public class TarefaActivity extends Debug {
 
-    private EditText edtDate;
+    private EditText edtDate, edtTime;
     private int year, month, day, hour, min;
     Calendar calendar;
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm ");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +27,18 @@ public class TarefaActivity extends Debug {
         setContentView(R.layout.activity_tarefa);
         edtDate = (EditText)findViewById(R.id.edt_date_tarefa);
         edtDate.setInputType(InputType.TYPE_NULL);
+        edtTime = (EditText)findViewById(R.id.edt_time_tarefa);
+        edtTime.setInputType(InputType.TYPE_NULL);
 
         //Abre o dialog do calendar com a data atual
         calendar = Calendar.getInstance();
         year = calendar.get(Calendar.YEAR);
         month = calendar.get(Calendar.MONTH);
         day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        //Abre o dialog do Time com a hora atual
+        hour = calendar.get(Calendar.HOUR_OF_DAY);
+        min = calendar.get(Calendar.MINUTE);
 
         edtDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,7 +53,9 @@ public class TarefaActivity extends Debug {
         datePickerDialog.show();
     }
 
-    public void showTime(){
+    public void showTime(View view){
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,onTimeSetListener,hour,min,true);
+        timePickerDialog.show();
     }
 
     private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
@@ -56,6 +66,16 @@ public class TarefaActivity extends Debug {
             calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
             String date = simpleDateFormat.format(calendar.getTime());
             edtDate.setText(date);
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int hour, int min) {
+            calendar.set(Calendar.HOUR_OF_DAY, hour);
+            calendar.set(Calendar.MINUTE, min);
+            String time = timeFormat.format(calendar.getTime());
+            edtTime.setText(time);
         }
     };
 }
